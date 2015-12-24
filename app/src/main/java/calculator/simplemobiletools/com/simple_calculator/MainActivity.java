67a33593c002e0b9076848b4ebda79c8c1b596e5
tibbi <tibor@kaputa.sk>
2015-12-24 12:28:00 +0100

    creating the android project

---
+++ app/src/main/java/calculator/simplemobiletools/com/simple_calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package calculator.simplemobiletools.com.simple_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
