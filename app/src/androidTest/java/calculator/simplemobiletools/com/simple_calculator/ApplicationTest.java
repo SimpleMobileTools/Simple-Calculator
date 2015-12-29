ca53937aef1895814634cf286f9832c8520f635f
tibbi <tibor@kaputa.sk>
2015-12-29 12:49:48 +0100

    implement an Espresso test

--- app/src/androidTest/java/calculator/simplemobiletools/com/simple_calculator/ApplicationTest.java
+++ app/src/androidTest/java/calculator/simplemobiletools/com/simple_calculator/ApplicationTest.java
@@ -3,9 +3,6 @@ package calculator.simplemobiletools.com.simple_calculator;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
