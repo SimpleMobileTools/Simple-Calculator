fb5ccc50dc64ede61fa3c41545204f639869b503
tibbi <tibor@kaputa.sk>
2016-01-01 21:13:04 +0100

    create a simple widget config activity

---
+++ app/src/main/java/calculator/simplemobiletools/com/simple_calculator/MyWidgetConfigure.java
@@ -0,0 +1,39 @@
+package calculator.simplemobiletools.com.simple_calculator;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MyWidgetConfigure extends Activity {
+    private int widgetId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.widget_config);
+        ButterKnife.bind(this);
+
+        final Intent intent = getIntent();
+        final Bundle extras = intent.getExtras();
+        if (extras != null) {
+            widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        if (widgetId == AppWidgetManager.INVALID_APPWIDGET_ID)
+            finish();
+
+        setResult(RESULT_CANCELED);
+    }
+
+    @OnClick(R.id.config_save)
+    public void saveConfig() {
+        final Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+        setResult(RESULT_OK, resultValue);
+        finish();
+    }
+}
