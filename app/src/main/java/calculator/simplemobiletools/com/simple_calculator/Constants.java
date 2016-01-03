4448b988cb7174fa806fc5848323eabb7c45757a
tibbi <tibor@kaputa.sk>
2016-01-03 17:26:10 +0100

    change a constants' value for consistency

--- app/src/main/java/calculator/simplemobiletools/com/simple_calculator/Constants.java
+++ app/src/main/java/calculator/simplemobiletools/com/simple_calculator/Constants.java
@@ -27,5 +27,5 @@ public class Constants {
 
     public static final String PREFS = "prefs";
     public static final String VALUE = "value";
-    public static final String WIDGET_BG_COLOR = "widget_bg";
+    public static final String WIDGET_BG_COLOR = "widget_bg_color";
 }
