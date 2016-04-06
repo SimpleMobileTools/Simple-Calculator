42f9815fd97e236f27f6a7ee7e10ca12472544cc
tibbi <tibor@kaputa.sk>
2016-04-06 20:22:06 +0200

    add an About activity

---
+++ app/src/main/java/com/simplemobiletools/calculator/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.simplemobiletools.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle(getResources().getString(R.string.about));
+    }
+}
--- app/src/main/java/com/simplemobiletools/calculator/MainActivity.java
+++ app/src/main/java/com/simplemobiletools/calculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.simplemobiletools.calculator;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -26,6 +30,25 @@ public class MainActivity extends AppCompatActivity implements Calculator {
         calc = new CalculatorImpl(this);
         setupResultView();
         AutofitHelper.create(result);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                final Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setupResultView() {
