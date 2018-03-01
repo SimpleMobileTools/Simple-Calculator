1dbfb792076429ddbde6acf2f531684c76568df3
arkondoma <arkondoma@gmail.com>
2018-03-01 16:38:42 -0500

    Creation of HistoryActivity, not yet connected to back end functionality

---
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/HistoryActivity.kt
@@ -0,0 +1,19 @@
+package com.simplemobiletools.calculator.activities
+
+import android.os.Bundle
+import com.simplemobiletools.calculator.R
+
+class HistoryActivity : SimpleActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+
+
+}
