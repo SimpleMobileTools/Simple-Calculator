4b3e0d477fb7700d66ebe3fe10ea6e61777a796d
marcdragon123 <thedragon_master@hotmail.ca>
2018-03-02 10:54:13 -0500

    #30 Adding UI components to create the activity for the history section
    Signed-off-by: marcdragon123 <thedragon_master@hotmail.ca>

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/HistoryActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/HistoryActivity.kt
@@ -3,6 +3,9 @@ package com.simplemobiletools.calculator.activities
 import android.os.Bundle
 import com.simplemobiletools.calculator.R
 
+/**
+ * Created by Marc-Andre Dragon on 2018-03-01.
+ */
 class HistoryActivity : SimpleActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
