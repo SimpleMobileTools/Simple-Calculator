92f5ccf980bdb66ce5214d5de269842c534dc2ec
tibbi <tibor@kaputa.sk>
2019-04-07 22:54:30 +0200

    updating the way the app is signed

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/MainActivity.kt
@@ -125,7 +125,8 @@ class MainActivity : SimpleActivity(), Calculator {
                 FAQItem(R.string.faq_1_title, R.string.faq_1_text),
                 FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
                 FAQItem(R.string.faq_4_title_commons, R.string.faq_4_text_commons),
-                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons)
+                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
+                FAQItem(R.string.faq_2_title_commons, R.string.faq_6_text_commons)
         )
 
         startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
