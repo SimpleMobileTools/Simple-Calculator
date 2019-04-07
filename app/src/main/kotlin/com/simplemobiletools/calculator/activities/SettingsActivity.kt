09cfa58bc2b4465b7d70cb27f055aca8f29e093e
tibbi <tibor@kaputa.sk>
2019-04-07 22:45:52 +0200

    updating commons, kotlin, gradle

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/SettingsActivity.kt
@@ -25,7 +25,6 @@ class SettingsActivity : SimpleActivity() {
         setupPurchaseThankYou()
         setupCustomizeColors()
         setupUseEnglish()
-        setupAvoidWhatsNew()
         setupVibrate()
         setupPreventPhoneFromSleeping()
         setupCustomizeWidgetColors()
@@ -52,14 +51,6 @@ class SettingsActivity : SimpleActivity() {
             settings_use_english.toggle()
             config.useEnglish = settings_use_english.isChecked
             System.exit(0)
-        }
-    }
-
-    private fun setupAvoidWhatsNew() {
-        settings_avoid_whats_new.isChecked = config.avoidWhatsNew
-        settings_avoid_whats_new_holder.setOnClickListener {
-            settings_avoid_whats_new.toggle()
-            config.avoidWhatsNew = settings_avoid_whats_new.isChecked
         }
     }
 
