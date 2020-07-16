673efc8e00139042ea0452e14feeaccc50df1f71
Vyas Nellutla <vyas@nifftytech.com>
2020-07-16 09:19:10 -0500

    Fix Division Operation

--- app/src/main/kotlin/com/simplemobiletools/calculator/operation/DivideOperation.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/operation/DivideOperation.kt
@@ -3,6 +3,7 @@ package com.simplemobiletools.calculator.operation
 import com.simplemobiletools.calculator.operation.base.BinaryOperation
 import com.simplemobiletools.calculator.operation.base.Operation
 import java.math.BigDecimal
+import java.math.MathContext.DECIMAL128
 
 class DivideOperation(baseValue: BigDecimal, secondValue: BigDecimal) : BinaryOperation(baseValue, secondValue), Operation {
 
@@ -10,7 +11,7 @@ class DivideOperation(baseValue: BigDecimal, secondValue: BigDecimal) : BinaryOp
         return if (secondValue.compareTo(BigDecimal.ZERO) == 0) {
             BigDecimal.ZERO
         } else {
-            baseValue.divide(secondValue)
+            baseValue.divide(secondValue, DECIMAL128)
         }
     }
 }
