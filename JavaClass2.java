1fa431fd672e489b120d08a56814a725aaa991d9
Lira1999 <nishatsadaflira@gmail.com>
2022-05-06 14:14:22 +0600

    1st branch committed

---
+++ JavaClass2.java
@@ -0,0 +1,18 @@
+package Assignment.Simple-Calculator_forked;
+
+public class JavaClass2 {
+  int a;
+
+  public JavaClass2(int a) {
+    this.a = a;
+  }
+
+  public int getA() {
+    return a;
+  }
+
+  public void setA(int a) {
+    this.a = a;
+  }
+  
+}
