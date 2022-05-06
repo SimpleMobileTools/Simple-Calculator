826ec61403cc2ff8903fbe86344008a5897e8ab8
Lira1999 <nishatsadaflira@gmail.com>
2022-05-06 14:04:07 +0600

    Java Class Committed

---
+++ JavaClass.java
@@ -0,0 +1,28 @@
+package Assignment.Simple-Calculator_forked;
+
+public class JavaClass {
+  int x, y;
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public JavaClass(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+
+}
