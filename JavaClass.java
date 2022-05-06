e98b79a4a1babc7a30aa8c7e2e222c39662357f0
Lira1999 <nishatsadaflira@gmail.com>
2022-05-06 14:25:49 +0600

    commit for 2nd branch

--- JavaClass.java
+++ JavaClass.java
@@ -1,10 +1,18 @@
 package Assignment.Simple-Calculator_forked;
 
 public class JavaClass {
-  int x, y;
+  int x, y, z;
 
   public int getX() {
     return x;
+  }
+
+  public int getZ() {
+    return z;
+  }
+
+  public void setZ(int z) {
+    this.z = z;
   }
 
   public void setX(int x) {
@@ -22,6 +30,10 @@ public class JavaClass {
   public JavaClass(int x, int y) {
     this.x = x;
     this.y = y;
+  }
+
+  public JavaClass(int z) {
+    this.z = z;
   }
   
 
