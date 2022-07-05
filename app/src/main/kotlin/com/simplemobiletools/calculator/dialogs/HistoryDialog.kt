b117c15ae67be227047c8ce335be54877a7776f5
tibbi <tibor@kaputa.sk>
2022-07-05 15:25:46 +0200

    updating commons

--- app/src/main/kotlin/com/simplemobiletools/calculator/dialogs/HistoryDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/dialogs/HistoryDialog.kt
@@ -12,23 +12,27 @@ import com.simplemobiletools.commons.extensions.toast
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import kotlinx.android.synthetic.main.dialog_history.view.*
 
-class HistoryDialog() {
-    constructor(activity: SimpleActivity, items: List<History>, calculator: CalculatorImpl) : this() {
+class HistoryDialog(activity: SimpleActivity, items: List<History>, calculator: CalculatorImpl) {
+    var dialog: AlertDialog? = null
+
+    init {
         val view = activity.layoutInflater.inflate(R.layout.dialog_history, null)
 
-        val dialog = AlertDialog.Builder(activity)
+        AlertDialog.Builder(activity)
             .setPositiveButton(R.string.ok, null)
             .setNeutralButton(R.string.clear_history) { _, _ ->
                 ensureBackgroundThread {
                     activity.applicationContext.calculatorDB.deleteHistory()
                     activity.toast(R.string.history_cleared)
                 }
-            }.create().apply {
-                activity.setupDialogStuff(view, this, R.string.history)
+            }.apply {
+                activity.setupDialogStuff(view, this, R.string.history) { alertDialog ->
+                    dialog = alertDialog
+                }
             }
 
         view.history_list.adapter = HistoryAdapter(activity, items, calculator) {
-            dialog.dismiss()
+            dialog?.dismiss()
         }
     }
 }
