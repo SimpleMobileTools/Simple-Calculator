cb63ddf7e8def86046d61e92dccda3410d10fdf2
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-09-14 10:17:02 +0200

    Reduce number of time units

--- app/src/main/kotlin/com/simplemobiletools/calculator/helpers/converters/TimeConverter.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/helpers/converters/TimeConverter.kt
@@ -38,24 +38,6 @@ object TimeConverter : Converter {
             factor = 0.001
         )
 
-        data object Microsecond : Unit(
-            nameResId = R.string.unit_time_microsecond,
-            symbolResId = R.string.unit_time_microsecond_symbol,
-            factor = 0.000001
-        )
-
-        data object Nanosecond : Unit(
-            nameResId = R.string.unit_time_nanosecond,
-            symbolResId = R.string.unit_time_nanosecond_symbol,
-            factor = 0.000000001
-        )
-
-        data object Picosecond : Unit(
-            nameResId = R.string.unit_time_picosecond,
-            symbolResId = R.string.unit_time_picosecond_symbol,
-            factor = 0.000000000001
-        )
-
         data object Day : Unit(
             nameResId = R.string.unit_time_day,
             symbolResId = R.string.unit_time_day_symbol,
@@ -68,88 +50,10 @@ object TimeConverter : Converter {
             factor = DAY * 7
         )
 
-        data object MonthFull : Unit(
-            nameResId = R.string.unit_time_month_full,
-            symbolResId = R.string.unit_time_month_full_symbol,
-            factor = DAY * 30
-        )
-
-        data object MonthHollow : Unit(
-            nameResId = R.string.unit_time_month_hollow,
-            symbolResId = R.string.unit_time_month_hollow_symbol,
-            factor = DAY * 29
-        )
-
-        data object MonthSynodic : Unit(
-            nameResId = R.string.unit_time_month_synodic,
-            symbolResId = R.string.unit_time_month_synodic_symbol,
-            factor = DAY * 29.530589
-        )
-
-        data object MonthGregorianAverage : Unit(
-            nameResId = R.string.unit_time_month_gregorian_average,
-            symbolResId = R.string.unit_time_month_gregorian_average_symbol,
-            factor = DAY * 30.346875
-        )
-
-        data object YearLeap : Unit(
-            nameResId = R.string.unit_time_year_leap,
-            symbolResId = R.string.unit_time_year_leap_symbol,
-            factor = DAY * 366
-        )
-
-        data object YearGregorian : Unit(
+        data object Year : Unit(
             nameResId = R.string.unit_time_year_gregorian,
             symbolResId = R.string.unit_time_year_gregorian_symbol,
             factor = GREGORIAN_YEAR
-        )
-
-        data object YearJulian : Unit(
-            nameResId = R.string.unit_time_year_julian,
-            symbolResId = R.string.unit_time_year_julian_symbol,
-            factor = DAY * 365.25
-        )
-
-        data object Jiffy : Unit(
-            nameResId = R.string.unit_time_jiffy,
-            symbolResId = R.string.unit_time_jiffy_symbol,
-            factor = 1 / 60.0
-        )
-
-        data object Moment : Unit(
-            nameResId = R.string.unit_time_moment,
-            symbolResId = R.string.unit_time_moment_symbol,
-            factor = 90.0
-        )
-
-        data object Fortnight : Unit(
-            nameResId = R.string.unit_time_fortnight,
-            symbolResId = R.string.unit_time_fortnight_symbol,
-            factor = DAY * 7 * 2
-        )
-
-        data object Decade : Unit(
-            nameResId = R.string.unit_time_decade,
-            symbolResId = R.string.unit_time_decade_symbol,
-            factor = GREGORIAN_YEAR * 10
-        )
-
-        data object Century : Unit(
-            nameResId = R.string.unit_time_century,
-            symbolResId = R.string.unit_time_century_symbol,
-            factor = GREGORIAN_YEAR * 100
-        )
-
-        data object Millennium : Unit(
-            nameResId = R.string.unit_time_millennium,
-            symbolResId = R.string.unit_time_millennium_symbol,
-            factor = GREGORIAN_YEAR * 1000
-        )
-
-        data object AtomicUnit : Unit(
-            nameResId = R.string.unit_time_atomic_unit,
-            symbolResId = R.string.unit_time_atomic_unit_symbol,
-            factor = 0.00000000000000002418884254
         )
     }
 
@@ -158,25 +62,9 @@ object TimeConverter : Converter {
         Unit.Minute,
         Unit.Second,
         Unit.Millisecond,
-        Unit.Microsecond,
-        Unit.Nanosecond,
-        Unit.Picosecond,
         Unit.Day,
         Unit.Week,
-        Unit.MonthFull,
-        Unit.MonthHollow,
-        Unit.MonthSynodic,
-        Unit.MonthGregorianAverage,
-        Unit.YearLeap,
-        Unit.YearGregorian,
-        Unit.YearJulian,
-        Unit.Jiffy,
-        Unit.Moment,
-        Unit.Fortnight,
-        Unit.Decade,
-        Unit.Century,
-        Unit.Millennium,
-        Unit.AtomicUnit
+        Unit.Year,
     )
 
     override val defaultTopUnit: Unit = Unit.Hour
